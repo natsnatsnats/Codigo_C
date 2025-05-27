f9a7050756a5b885e5edf4ce60a29c2b04dadc2c
Natan Pena <natscoisaseria@gmail.com>
2025-05-27 09:07:49 -0300

    Create codigo_media.c

---
+++ codigo_media.c
@@ -0,0 +1,11 @@
+int main() {
+  float numUm, numDois, media = 0;
+    printf("Digite o primeiro número: \n");
+    scanf("%f", &numUm);
+    printf("Digite o segundo número: \n");
+    scanf("%f", &numDois);
+
+  media = (numUm + numDois)/2;
+  
+  printf("A média dos números é igual a: %f", media);
+}
