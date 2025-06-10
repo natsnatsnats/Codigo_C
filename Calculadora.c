84949a55a56ca20891f717e0ceaa42af75d57e25
Natan Pena <natscoisaseria@gmail.com>
2025-06-10 09:25:29 -0300

    Update Calculadora.c

--- Calculadora.c
+++ Calculadora.c
@@ -1,29 +1,51 @@
-#include <stdio.h> 
+#include <stdio.h>
+#include <math.h>
 
 int main() {
-    char op;    
+    int escolha;
     float n1, n2, resultado;
-  
-    printf("Entre com a operacao (+, -, * ou /) e dois numeros reais:\n");
-    scanf(" %c %f %f", &op, &n1, &n2);
 
-    if (op == '+') {
-        res = n1 + n2;
-        printf("O resultado: %.2f\n", resultado);
-    } else if (op == '-') {
-        res = n1 - n2;
-        printf("O resultado: %.2f\n", resultado);
-    } else if (op == '*') {
-        res = n1 * n2;
-        printf("O resultado: %.2f\n", resultado);
-    } else if (op == '/') {
-        if (n2 != 0) {
-            res = n1 / n2;
-            printf("O resultado: %.2f\n", resultado);
-        } else {
-            printf("Divisao por zero? sério?\n");
-        }
-    } else {
-        printf("A operacao invalida!\n");
+    printf("Escolha a operacao:\n1-Soma\n2-Subtracao\n3-Multiplicacao\n4-Divisao\n5-Raiz Quadrada\n6-Potencia\nOpcao: ");
+    scanf("%d", &escolha);
+
+    if (escolha >= 1 && escolha <= 4 || escolha == 6) {
+        printf("Entre com dois numeros: ");
+        scanf("%f %f", &n1, &n2);
+    } else if (escolha == 5) { 
+        printf("Entre com um numero: ");
+        scanf("%f", &n1);
     }
+
+    if (escolha == 1) { 
+        resultado = n1 + n2;
+    } else if (escolha == 2) { 
+        resultado = n1 - n2;
+    } else if (escolha == 3) { 
+        resultado = n1 * n2;
+    } else if (escolha == 4) { 
+        if (n2 != 0) {
+            resultado = n1 / n2;
+        } else {
+            printf("Erro: Divisao por zero!\n");
+            return 1;
+        }
+    } else if (escolha == 5) { 
+        if (n1 >= 0) {
+            resultado = sqrt(n1);
+        } else {
+            printf("Erro: Raiz de numero negativo!\n");
+            return 1; 
+        }
+    } else if (escolha == 6) { 
+        resultado = pow(n1, n2);
+    } else { 
+        printf("Opcao invalida!\n");
+        return 1; 
+    }
+
+    if (escolha >= 1 && escolha <= 6 && (escolha != 4 || n2 != 0) && (escolha != 5 || n1 >= 0)) {
+        printf("Resultado: %.2f\n", resultado);
+    }
+
+    return 0;
 }
